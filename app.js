3d9333b3ed2ce81abe9a664d0b25fb96d0a602eb
Batuhan Ulas <ulas@sklib-procomputer.de>
2023-02-17 19:07:11 +0100

    created orders Route

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 const productsRoutes = require("./routes/products");
+const orderRoutes = require("./routes/orders");
 
 app.use("/products", productsRoutes);
+app.use("/orders", orderRoutes);
 
 module.exports = app;
